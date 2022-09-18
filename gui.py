49a56b5df6126e4cf81c65d2b7d68e25efb43d77
SahilRajputdotexe <ajstylespheno@gmail.com>
2022-09-18 23:16:32 +0530

    importiing all of tinker

--- gui.py
+++ gui.py
@@ -1,12 +1,15 @@
-import tkinter as T
+from tkinter import *
 
-root = T.Tk()
+root = Tk()
 
 root.geometry('1200x800')
-
+root.iconbitmap("D:\cpp\projectbase1\Paomedia-Small-N-Flat-Key.ico")
 root.title("PASS SAVE")
 
-label = T.Label()
+label = Label()
 
+button1 = Button(master=root, width="10", text="SAVE",
+                 activebackground='red')
 
+button1.place(x=50, y=50)
 root.mainloop()
